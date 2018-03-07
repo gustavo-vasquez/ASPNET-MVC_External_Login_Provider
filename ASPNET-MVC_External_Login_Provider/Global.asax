801dc0724a5877b0ca1653fdb97cc826e264b62f
gustavo-vasquez <vasquez.gustavo90@gmail.com>
2018-03-07 13:33:24 -0300

    Primer commit del proyecto

---
+++ ASPNET-MVC_External_Login_Provider/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASPNET_MVC_External_Login_Provider.MvcApplication" Language="C#" %>
---
+++ ASPNET-MVC_External_Login_Provider/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPNET_MVC_External_Login_Provider.Startup))]
+namespace ASPNET_MVC_External_Login_Provider
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
